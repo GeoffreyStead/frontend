0fd08260e0174c4fe3fe73bfc76c1cdf186ff59f
Geoffrey <Geoffrey.stead@gmail.com>
2024-05-07 14:03:52 +0200

    New logo

--- components/molecules/nav-bar/NavBar.js
+++ components/molecules/nav-bar/NavBar.js
@@ -4,7 +4,7 @@ import { NavBarWrapper } from "./NavBar.styles";
 export function NavBar({ setSelected }) {
   return (
     <NavBarWrapper>
-      <div className="tab" onClick={() => setSelected("upload")}>Title</div>
+      <div className="tab" onClick={() => setSelected("upload")}><img src="CSVReaderLogo.png"></img></div>
       <div className="tab" onClick={() => setSelected("upload")}>Upload</div>
       <div className="tab" onClick={() => setSelected("data")}>Data</div>
       <div className="tab" onClick={() => setSelected("graph")}>Graph</div>
